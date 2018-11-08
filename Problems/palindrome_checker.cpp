3e60d22f8ef6c0ba2d2df92ac8086a0c2ac2d627
Vanik <vaniksarajyan@gmail.com>
2018-11-08 16:09:15 +0400

    palndrome checker

---
+++ Problems/palindrome_checker.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string s){
+    for(unsigned int i = 0; i < s.length() / 2; i++){
+        if(s[i] != s[s.length() - 1 - i]){
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    isPalindrome(s) ? cout << "Yes \n" : cout << "No \n";
+}
